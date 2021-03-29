3fc606c53e0e6b149bf9b15814bfa3b4561996ab
menega2010 <60823553+menega2010@users.noreply.github.com>
2021-03-28 21:34:04 -0300

    Atuallizadoo
    Atuallizadoo

--- main.py
+++ main.py
@@ -1,24 +1,27 @@
 from calculadora import Calculadora
+from comodo import Comodo
+
 
 calc = Calculadora()
 
-largura: float = float(input('Qual a largura do cômodo ? '))  
-profundidade: float = float(input('Qual a profundidade do cômodo ? ')) 
-altura: float = 2.9
+comodo = Comodo(
+    input('Qual a largura do cômodo ? '),
+    input('Qual a profundidade do cômodo ? ')
+)
 
 # calc.area_paredes: float = 2 * (largura + profundidade) * altura
 
 
 print(
     'A area das paredes é > ' , 
-        calc.calcular_area_parede(largura, profundidade, altura)
+        calc.calcular_area_parede(comodo)
 )
 
 # calc.area_teto: float = largura * profundidade
 
 print(
     'A área do teto é > ',
-        calc.calcula_area_teto(largura, profundidade)
+        calc.calcula_area_teto(comodo)
 )
 
 print(
